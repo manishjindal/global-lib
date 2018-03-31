4d201981d5f3d47e9dc875cabe61092ee5600096
mjindal <manish_jindal@mentor.com>
2018-03-31 14:21:05 +0530

    added cps lib

--- src/com/mjindal/devops/DockerLogin.groovy
+++ src/com/mjindal/devops/DockerLogin.groovy
@@ -1,9 +1,10 @@
 package com.mjindal.devops
+import com.cloudbees.groovy.cps.NonCPS
 
 def call(String username){
     return getUsername(username);
 }
-
+@NonCPS
 String getUsername(username){
     return username;
 }
