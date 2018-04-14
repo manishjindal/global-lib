47f67229f44ada8dbaa1e63497f99373c935f925
mjindal <manish_jindal@mentor.com>
2018-04-14 09:49:04 +0530

    added new function to lib

--- src/com/mjindal/devops/Setup.groovy
+++ src/com/mjindal/devops/Setup.groovy
@@ -10,7 +10,7 @@ class Setup implements Serializable {
     }
 
     def loginToDockerHub(){
-        withCredentials([usernameColonPassword(credentialsId: '8d3f3ee8-05dd-49f3-9cdd-f21d3119784d', variable: 'TOKEN')]) {
+        steps.withCredentials([usernameColonPassword(credentialsId: '8d3f3ee8-05dd-49f3-9cdd-f21d3119784d', variable: 'TOKEN')]) {
             // some block
         }
     }
